b41a0d298d71e99750d0c88cde4661895f71c5f4
Ben Arnold <ben@arnoldfamily.me.uk>
2019-10-18 11:58:56 +0100

    Accept ticket ID via POST in `NotifyTicket`

--- src/SFA.DAS.Zendesk.Monitor.Function/ZenWatchDurableFunction.cs
+++ src/SFA.DAS.Zendesk.Monitor.Function/ZenWatchDurableFunction.cs
@@ -3,7 +3,9 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,14 +19,24 @@ namespace ZenWatchFunction
         private static readonly string WatcherInstance = "{8B2772F1-0A07-4D64-BEBE-1402520C0BD0}";
         private static readonly RetryOptions retry = new RetryOptions(TimeSpan.FromSeconds(1), 5);
 
-        [FunctionName("ClientFunction")]
+        private class NotifyTicket
+        {
+            public long Id { get; set; }
+        }
+
+        [FunctionName("NotifyTicket")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequest request,
             [OrchestrationClient]DurableOrchestrationClient starter,
             ILogger log)
         {
-            var ids = request.Query["id"].Select(x => long.Parse(x)).ToArray();
-            log.LogInformation("Sharing ticket {id}", ids);
+            var ids = request.Query["id"].Select(x => long.Parse(x)).ToList();
+
+            var content = await new StreamReader(request.Body).ReadToEndAsync();
+            var ticket = JsonConvert.DeserializeObject<NotifyTicket>(content);
+            if(ticket?.Id != null) ids.Add(ticket.Id);
+
+            log.LogInformation("NotifyTicket {ids}", ids);
 
             var instanceId = await starter.StartNewAsync(nameof(ShareListedTickets), ids);
             return starter.CreateCheckStatusResponse(request, instanceId);
