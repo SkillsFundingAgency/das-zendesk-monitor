018e28471a555453caf16f17276d756c68b057a8
Ben Arnold <ben@arnoldfamily.me.uk>
2019-12-05 15:06:49 +0000

    Test the minimum data possible from Zendesk passes schema validation

--- src/SFA.DAS.Zendesk.Monitor.UnitTests/Tests/MiddlewareSchemaTestData.cs
+++ src/SFA.DAS.Zendesk.Monitor.UnitTests/Tests/MiddlewareSchemaTestData.cs
@@ -32,6 +32,56 @@ namespace SFA.DAS.Zendesk.Monitor.UnitTests.Tests
                 ticket.Organizations[0].OrganizationFields.AddressLine3 = null;
                 yield return ticket;
             }
+
+            {
+                var ticket = DefaultZendeskTicket();
+                ticket.Users[0].UserFields.AddressLine1 = null;
+                ticket.Users[0].UserFields.AddressLine2 = null;
+                ticket.Users[0].UserFields.AddressLine3 = null;
+                yield return ticket;
+            }
+
+            {
+                var minimalTicket = new TicketResponse
+                {
+                    Ticket = new Ticket
+                    {
+                        RequesterId = 1,
+                        OrganizationId = 1,
+                        Subject = "Ticket ticket ticket",
+                        Description = "...",
+                        Via = new Via
+                        {
+                            Channel = "email",
+                        }
+                    },
+                    Users = new []
+                    {
+                        new User
+                        {
+                            Id = 1,
+                            Name = "me",
+                            UserFields = new UserFields { },
+                        }
+                    },
+                    Organizations = new []
+                    {
+                        new Organization
+                        {
+                            Id = 1,
+                            Name = "BobCo",
+                            OrganizationFields = new OrganizationFields 
+                            {
+                                // TODO - this may be null / empty from Zendesk,
+                                //        but the schema mandates it's one of 
+                                //        its enum values
+                                OrganisationStatus = "active",
+                            },
+                        },
+                    },
+                };
+                yield return minimalTicket;
+            }
         }
 
         private static TicketResponse DefaultZendeskTicket()
@@ -42,8 +92,8 @@ namespace SFA.DAS.Zendesk.Monitor.UnitTests.Tests
             ticket.Ticket.Via.Channel = "email";
 
             ticket.Ticket.OrganizationId = ticket.Organizations[0].Id;
-            ticket.Organizations[0].OrganizationFields.OrganizationType = "";
-            ticket.Organizations[0].OrganizationFields.OrganizationStatus = "active";
+            ticket.Organizations[0].OrganizationFields.OrganisationType = "";
+            ticket.Organizations[0].OrganizationFields.OrganisationStatus = "active";
 
             ticket.Ticket.RequesterId = ticket.Users[0].Id;
             ticket.Users[0].UserFields.ContactType = "";
--- src/SFA.DAS.Zendesk.Monitor/TicketProfile.cs
+++ src/SFA.DAS.Zendesk.Monitor/TicketProfile.cs
@@ -8,17 +8,17 @@ namespace SFA.DAS.Zendesk.Monitor
         public TicketProfile()
         {
             CreateMap<Zendesk.Model.TicketResponse, Middleware.EventWrapper>()
-                .ForMember(x => x.Ticket, x => x.MapFrom(y => y.Ticket))
-                .ForPath(x => x.Ticket.Comments, x => x.MapFrom(y => y.Comments))
-                .ForPath(x => x.Ticket.Requester, x => x.MapFrom(y => FindRequester(y)))
-                .ForPath(x => x.Ticket.Organization, x => x.MapFrom(y => FindOrganisation(y)))
+                .ForMember(x => x.Ticket, x => x.MapFrom(p => p.Ticket))
+                .ForPath(x => x.Ticket.Comments, x => x.MapFrom(p => p.Comments))
+                .ForPath(x => x.Ticket.Requester, x => x.MapFrom(p => FindRequester(p)))
+                .ForPath(x => x.Ticket.Organization, x => x.MapFrom(p => FindOrganisation(p)))
                 ;
 
             CreateMap<Zendesk.Model.Ticket, Middleware.Model.Ticket>()
-                .ForMember(x => x.Comments, x => x.Ignore())
-                .ForMember(x => x.Requester, x => x.Ignore())
-                .ForMember(x => x.Organization, x => x.Ignore())
-                .ForPath(x => x.Via, x => x.MapFrom(y => TranslateVia(y)))
+                .ForMember(dest => dest.Comments, src => src.Ignore())
+                .ForMember(dest => dest.Requester, src => src.Ignore())
+                .ForMember(dest => dest.Organization, src => src.Ignore())
+                .ForPath(dest => dest.Via, src => src.MapFrom(p => TranslateVia(p)))
                 ;
 
             CreateMap<Zendesk.Model.Comment, Middleware.Model.Comments>();
@@ -27,10 +27,24 @@ namespace SFA.DAS.Zendesk.Monitor
                 .ForMember(x => x.Url, x => x.MapFrom(y => y.ContentUrl))
                 ;
             CreateMap<Zendesk.Model.Field, Middleware.Model.CustomField>();
-            CreateMap<Zendesk.Model.User, Middleware.Model.User>();
+
+            CreateMap<Zendesk.Model.User, Middleware.Model.User>()
+                .ForMember(dest => dest.Email, src => src.NullSubstitute(""))
+                .ForMember(dest => dest.Phone, src => src.NullSubstitute(""))
+                ;
+
+            CreateMap<Zendesk.Model.UserFields, Middleware.Model.UserFields>()
+                .ForMember(dest => dest.AddressLine1, src => src.NullSubstitute(""))
+                .ForMember(dest => dest.AddressLine2, src => src.NullSubstitute(""))
+                .ForMember(dest => dest.AddressLine3, src => src.NullSubstitute(""))
+                .ForAllOtherMembers(src => src.NullSubstitute(""))
+                ;
+
             CreateMap<Zendesk.Model.Organization, Middleware.Model.Organisation>();
-            CreateMap<Zendesk.Model.UserFields, Middleware.Model.UserFields>();
-            CreateMap<Zendesk.Model.OrganizationFields, Middleware.Model.OrganizationFields>();
+            CreateMap<Zendesk.Model.OrganizationFields, Middleware.Model.OrganizationFields>()
+                .ForMember(dest => dest.MainPhone, src => src.MapFrom(p => p.ToString() ?? ""))
+                .ForAllOtherMembers(src => src.NullSubstitute(""))
+                ;
         }
 
         private Zendesk.Model.Organization FindOrganisation(Zendesk.Model.TicketResponse response)
