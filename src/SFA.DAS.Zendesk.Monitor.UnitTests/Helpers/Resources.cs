e58bffccc7e6d19d5ff0309e13152b2a9bccb189
Ben Arnold <ben@arnoldfamily.me.uk>
2019-12-12 17:20:57 +0000

    🔨 Move test helpers into a directory

--- src/SFA.DAS.Zendesk.Monitor.UnitTests/Helpers/Resources.cs
+++ src/SFA.DAS.Zendesk.Monitor.UnitTests/Helpers/Resources.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-namespace SFA.DAS.Zendesk.Monitor.UnitTests.Tests
+namespace SFA.DAS.Zendesk.Monitor.UnitTests
 {
     public class Resources
     {
