c84cba046d8d319bd91e52d400e4ca32bf791dea
Ben Arnold <ben@arnoldfamily.me.uk>
2019-12-16 12:38:35 +0000

    🔨 Resolve config once in startup

--- src/SFA.DAS.Zendesk.Monitor.Function/Startup.cs
+++ src/SFA.DAS.Zendesk.Monitor.Function/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.Zendesk.Monitor;
 using System;
-using System.Linq;
 using MW = SFA.DAS.Zendesk.Monitor.Middleware;
 using ZD = SFA.DAS.Zendesk.Monitor.Zendesk;
 
@@ -22,24 +21,30 @@ namespace ZenWatchFunction
             builder.Services.AddTransient<DurableWatcher>();
             builder.Services.AddTransient<Watcher>();
             builder.Services.AddTransient<ZD.SharingTickets>();
-            builder.Services.AddTransient<ZD.ISharingTickets>(s => s.GetRequiredService<ZD.SharingTickets>());
+            builder.Services.AddTransient<ZD.ISharingTickets>(
+                s => s.GetRequiredService<ZD.SharingTickets>());
             builder.Services.AddSingleton(s =>
             {
-                var config = s.GetRequiredService<IConfiguration>();
                 var logger = s.GetRequiredService<ILogger<LoggingHttpClientHandler>>();
-
-                var all = config.GetChildren().Select(x => x.Key);
-
-                return new ZD.ApiFactory(new Uri(config["Zendesk:Url"]), config["Zendesk:ApiUser"], config["Zendesk:ApiKey"], logger);
+                return new ZD.ApiFactory(
+                    new Uri(config["Zendesk:Url"]),
+                    config["Zendesk:ApiUser"],
+                    config["Zendesk:ApiKey"],
+                    logger);
             });
-            builder.Services.AddTransient(s => s.GetRequiredService<ZD.ApiFactory>().CreateApi());
+            builder.Services.AddTransient(s =>
+                s.GetRequiredService<ZD.ApiFactory>().CreateApi());
             builder.Services.AddTransient(s =>
             {
-                var config = s.GetRequiredService<IConfiguration>();
                 var logger = s.GetRequiredService<ILogger<LoggingHttpClientHandler>>();
-                return new MW.ApiFactory(new Uri(config["Middleware:Url"]), config["Middleware:SubscriptionKey"], config["Middleware:ApiBasicAuth"], logger);
+                return new MW.ApiFactory(
+                    new Uri(config["Middleware:Url"]),
+                    config["Middleware:SubscriptionKey"],
+                    config["Middleware:ApiBasicAuth"],
+                    logger);
             });
-            builder.Services.AddTransient(s => s.GetRequiredService<MW.ApiFactory>().Create());
+            builder.Services.AddTransient(s =>
+                s.GetRequiredService<MW.ApiFactory>().Create());
             builder.Services.AddLogging(config);
         }
     }
