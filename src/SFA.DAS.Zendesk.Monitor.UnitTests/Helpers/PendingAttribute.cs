551ff47f9ccc6ada90615f4715da2f8ba484241b
Ben Arnold <ben@arnoldfamily.me.uk>
2020-01-13 14:04:13 +0000

    🔨Simplify use of Pending test attribute

--- src/SFA.DAS.Zendesk.Monitor.UnitTests/Helpers/PendingAttribute.cs
+++ src/SFA.DAS.Zendesk.Monitor.UnitTests/Helpers/PendingAttribute.cs
@@ -1,50 +1,77 @@
 ﻿using AutoFixture;
+using AutoFixture.Dsl;
+using AutoFixture.Kernel;
 using AutoFixture.Xunit2;
 using SFA.DAS.Zendesk.Monitor.Zendesk.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using CustomisationFunc = System.Func<
+    AutoFixture.IFixture,
+    AutoFixture.Dsl.IPostprocessComposer<SFA.DAS.Zendesk.Monitor.Zendesk.Model.Ticket>,
+    AutoFixture.Dsl.IPostprocessComposer<SFA.DAS.Zendesk.Monitor.Zendesk.Model.Ticket>>;
 
-namespace SFA.DAS.Zendesk.Monitor.UnitTests.AutoFixture
+namespace SFA.DAS.Zendesk.Monitor.UnitTests.AutoFixtureCustomisation
 {
-    public enum As
+    public abstract class Pending
     {
-        Solved,
-        Escalated,
-    }
-
-    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
-    public class PendingAttribute : CustomizeAttribute
-    {
-        private As solved;
-
-        public PendingAttribute(As solved)
+        [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+        public class Solved : PendingAttribute
         {
-            this.solved = solved;
-        }
-
-        public override ICustomization GetCustomization(ParameterInfo parameter)
-        {
-            if (parameter.ParameterType != typeof(Ticket))
-                throw new InvalidOperationException($"`{parameter.ParameterType.Name}` is not a valid type for [Pending].");
-            return new PendingTicketCustomisation(solved);
-        }
-
-        private class PendingTicketCustomisation : ICustomization
-        {
-            private string reason;
-
-            public PendingTicketCustomisation(As reason)
+            public Solved()
+                : base(nameof(Solved))
             {
-                this.reason = reason.ToString().ToLower();
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+        public class Escalated : PendingAttribute
+        {
+            public Escalated()
+                : base(nameof(Escalated))
+            {
+            }
+        }
+
+        public abstract class PendingAttribute : CustomizeAttribute, ICustomization
+        {
+            private readonly List<CustomisationFunc> customisations = new List<CustomisationFunc>();
+
+            protected PendingAttribute(string reason)
+            {
+                customisations.Add((fixture, x) => AddTagCustomisation(fixture, x, reason));
+            }
+
+            private IPostprocessComposer<Ticket> AddTagCustomisation(
+                IFixture _,
+                IPostprocessComposer<Ticket> ticket, string reason)
+            =>
+                ticket
+                    .Without(y => y.Tags)
+                    .Do(y => y.Tags = new List<string> { $"pending_middleware_{reason.ToLower()}" });
+
+            public override ICustomization GetCustomization(ParameterInfo parameter)
+            {
+                if (parameter.ParameterType != typeof(Ticket))
+                    throw new InvalidOperationException(
+                        $"`{parameter.ParameterType.Name}` is not a valid type for [Pending].");
+                return this;
             }
 
             public void Customize(IFixture fixture)
-            {
-                fixture.Customize<Ticket>(x => x
-                    .Without(y => y.Tags)
-                    .Do(y => y.Tags = new List<string> { $"pending_middleware_{reason}" }));
-            }
+                => fixture.Customize(ApplyAllCustomisations(fixture));
+
+            private Func<ICustomizationComposer<Ticket>, ISpecimenBuilder> ApplyAllCustomisations(IFixture fixture)
+                => composer => ApplyAllCustomisations(fixture, composer);
+
+            private IPostprocessComposer<Ticket> ApplyAllCustomisations(
+                IFixture fixture,
+                ICustomizationComposer<Ticket> composer)
+            =>
+                customisations.Aggregate(
+                    (IPostprocessComposer<Ticket>)composer,
+                    (c, f) => f(fixture, c));
         }
     }
 }
