24d4fd042e59ef1cd59a272a8a02553255618291
Furqan Agwan <19633375+furqanagwan@users.noreply.github.com>
2025-01-20 15:51:26 +0000

    Test test

--- src/SFA.DAS.Zendesk.Monitor.UnitTests/Tests/ModelSerialisationTests.Zendesk.cs
+++ src/SFA.DAS.Zendesk.Monitor.UnitTests/Tests/ModelSerialisationTests.Zendesk.cs
@@ -1,10 +1,6 @@
-using AutoFixture.Xunit2;
 using FluentAssertions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using SFA.DAS.Zendesk.Monitor.Zendesk.Model;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -12,89 +8,19 @@ namespace SFA.DAS.Zendesk.Monitor.UnitTests
 {
     public class ModelSerialisationTests
     {
-        private const string iso8601 = "yyyy-MM-ddTHH:mm:ss.FFFFFFFK";
-
-        [Fact]
-        public void SmokeTestDeserialisationOfCapturedTicket()
-        {
-            var a = Resources.LoadAsString(
-                "SFA.DAS.Zendesk.Monitor.UnitTests.TestData.zendesk.ticket.1017.json");
-
-            var j = JsonConvert.DeserializeObject<Zendesk.Model.TicketResponse>(
-                a, Zendesk.ApiFactoryExtensions.serialiser);
-
-            j.Ticket.Should().BeEquivalentTo(new
-            {
-                Id = 1017,
-                CreatedAt = DateTimeOffset.Parse("2019-12-10T15:35:49Z"),
-                UpdatedAt = DateTimeOffset.Parse("2019-12-10T17:03:24Z"),
-                Type = "question",
-                Subject = "SQRL TEST - CRM SOLVE Test",
-                Description = "test",
-                Priority = "low",
-                Status = "solved",
-                RequesterId = 363003813860,
-                OrganizationId = 362277482460,
-                Tags = new[] { "hmrc____government_gateway_paye_link", "query" },
-            });
-
-            j.Ticket.CustomFields.Should().ContainEquivalentOf(
-                new { Id = 360004171439, Value = "INC01167381" });
-
-            j.Users.Should().ContainEquivalentOf(new
-            {
-                Id = 362626196619,
-                Name = "Shakil Mahmood",
-                Email = "shakil.mahmood@digital.education.gov.uk",
-                CreatedAt = DateTimeOffset.Parse("2019-09-04T19:26:12Z"),
-                UpdatedAt = DateTimeOffset.Parse("2019-12-12T15:11:26Z"),
-                Phone = "01215555555",
-                UserFields = new
-                {
-                    AddressLine1 = "12b Baker Street",
-                    AddressLine2 = "Somewhere",
-                    AddressLine3 = "",
-                    City = "Hull",
-                    Postcode = "hu1 1uh",
-                    ContactType = "employer",
-                }
-            });
-
-            j.Organizations.Should().ContainEquivalentOf(new
-            {
-                Name = "ShakTestOrg",
-                CreatedAt = DateTimeOffset.Parse("2019-10-07T13:51:06Z"),
-                UpdatedAt = DateTimeOffset.Parse("2019-12-10T16:13:58Z"),
-                OrganizationFields = new
-                {
-                    AddressLine1 = "123",
-                    AddressLine2 = "testing street",
-                    AddressLine3 = "north london",
-                    City = "harringey",
-                    County = "Armagh",
-                    Postcode = "n4 1qx",
-                    MainPhone = "01234567890",
-                    OrganisationStatus = "active",
-                    OrganisationType = "employer",
-                },
-            });
-        }
-
+        // Test the custom deserialization of event values.
         [Theory]
         [InlineData(@"""123456""", "123456")]
         [InlineData(@"[""123"", ""456""]", "123,456")]
-        [InlineData(@"{""minutes"":3600,""in_business_hours"":true}", 
-            "{\"minutes\": 3600, \"in_business_hours\": true}")]
+        [InlineData(@"{""minutes"":3600,""in_business_hours"":true}", "{\"minutes\": 3600, \"in_business_hours\": true}")]
         [InlineData(@"123456", "123456")]
-        [InlineData(@"null", null)]
-        public void TestCustomDeserialisationOfEventValue2(
-            string value,
-            string expected)
+        [InlineData(@"null", null)]  // Handling null value explicitly
+        public void TestCustomDeserializationOfEventValue2(string value, string expected)
         {
             var json = @"{""audits"": [{""events"": [" +
-                @"{""value"": " + value + "}," +
-                @"{""previous_value"": " + value + "}" +
-                @"]}]}";
+                        @"{""value"": " + value + "}," +
+                        @"{""previous_value"": " + value + "}" +
+                        @"]}]}";
 
             var j = JsonConvert.DeserializeObject<Zendesk.Model.AuditResponse>(
                 json, Zendesk.ApiFactoryExtensions.serialiser);
@@ -106,64 +32,66 @@ namespace SFA.DAS.Zendesk.Monitor.UnitTests
             j.Audits.SelectMany(x => x.Events).Should()
                 .ContainEquivalentOf(new { Value = expectedValue });
         }
-        
-        [Fact]
-        public void Main_phone_with_alpha_characters()
+
+        // Additional test cases for handling different event values
+        [Theory]
+        [InlineData(@"""123456""", "123456")]
+        [InlineData(@"[ ""123"", ""456"" ]", "123,456")]
+        [InlineData(@"null", null)]
+        public void TestValueHandling(string value, string expected)
         {
-            var a = Resources.LoadAsString(
-                "SFA.DAS.Zendesk.Monitor.UnitTests.TestData.zendesk.ticket.1086.main_phone_with_alpha.json");
+            var json = @"{""audits"": [{""events"": [" +
+                        @"{""value"": " + value + "}" +
+                        @"]}]}";
 
-            var j = JsonConvert.DeserializeObject<Zendesk.Model.TicketResponse>(
-                a, Zendesk.ApiFactoryExtensions.serialiser);
+            var j = JsonConvert.DeserializeObject<Zendesk.Model.AuditResponse>(
+                json, Zendesk.ApiFactoryExtensions.serialiser);
 
-            j.Users.Should().ContainEquivalentOf(new
-            {
-                Id = 6619,
-                Phone = "01234 666 777 and some letters",
-            });
+            // Handle null case
+            JToken expectedValue = expected == null ? null : JToken.Parse(expected);
 
-            j.Organizations.Should().ContainEquivalentOf(new
-            {
-                Name = "Org with alphabetic Main Phone field",
-                OrganizationFields = new
-                {
-                    MainPhone = "01234 567 890 and some letters",
-                },
-            });
+            // Ensure that the value matches the expected value
+            j.Audits.SelectMany(x => x.Events).Should()
+                .ContainEquivalentOf(new { Value = expectedValue });
         }
 
-        [Theory, AutoData]
-        public void Serialise_SafeModifyTags(SafeModifyTags ticket)
+        // Test with different event data
+        [Theory]
+        [InlineData(@"[{""value"":""12345""},{""value"":""67890""}]", "[\"12345\", \"67890\"]")]
+        [InlineData(@"[{""value"":""abc""},{""value"":""def""}]", "[\"abc\", \"def\"]")]
+        public void TestMultipleEvents(string value, string expected)
         {
-            var json = JsonConvert.SerializeObject(
-                ticket, Zendesk.ApiFactoryExtensions.serialiser);
+            var json = @"{""audits"": [{""events"": " + value + "}]}";
 
-            var expected = $"{{\"ticket\":{{" +
-                $"\"safe_update\":true," +
-                $"\"updated_stamp\":\"{ticket.UpdatedStamp.ToString(iso8601)}\"," +
-                $"\"tags\":[{ticket.Tags.ToQuotedList()}]" +
-                $"}}}}";
+            var j = JsonConvert.DeserializeObject<Zendesk.Model.AuditResponse>(
+                json, Zendesk.ApiFactoryExtensions.serialiser);
 
-            json.Should().BeEquivalentTo(expected);
+            // Ensure the events match the expected array
+            JArray expectedArray = JArray.Parse(expected);
+
+            j.Audits.SelectMany(x => x.Events).Select(e => e.Value).Should()
+                .ContainEquivalentOf(expectedArray);
         }
 
-        [Fact]
-        public void Serialise_CustomFields_with_complex_object()
+        // Test deserialization with complex objects (objects inside arrays)
+        [Theory]
+        [InlineData(@"{""minutes"":3600,""in_business_hours"":true}", "{\"minutes\": 3600, \"in_business_hours\": true}")]
+        [InlineData(@"{""status"":""active"",""count"":42}", "{\"status\":\"active\", \"count\": 42}")]
+        public void TestComplexObjectHandling(string value, string expected)
         {
-            var a = Resources.LoadAsString(
-                "SFA.DAS.Zendesk.Monitor.UnitTests.TestData.zendesk.ticket.39702.json");
+            var json = @"{""audits"": [{""events"": [" +
+                        @"{""value"": " + value + "}" +
+                        @"]}]}";
 
-            var j = JsonConvert.DeserializeObject<Zendesk.Model.TicketResponse>(
-                a, Zendesk.ApiFactoryExtensions.serialiser);
+            var j = JsonConvert.DeserializeObject<Zendesk.Model.AuditResponse>(
+                json, Zendesk.ApiFactoryExtensions.serialiser);
 
-            j.Ticket.CustomFields.Should().ContainEquivalentOf(
-                new { Id = 360002764900, Value = new JArray("7") });
+            // Deserialize the expected value as a JToken for flexible comparison
+            JToken expectedValue = JToken.Parse(expected);
+
+            // Ensure that the complex value matches the expected structure
+            j.Audits.SelectMany(x => x.Events).Should()
+                .ContainEquivalentOf(new { Value = expectedValue });
         }
-    }
-
-    public static class StringExtensions
-    {
-        public static string ToQuotedList(this IEnumerable<string> xs)
-            => string.Join(",", xs.Select(x => $"\"{x}\""));
     }
 }
