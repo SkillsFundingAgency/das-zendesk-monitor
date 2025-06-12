372010ed99acffb97b70d86348579d020cde7259
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-06-12 15:14:37 +0100

    Added notify testcases

---
+++ src/SFA.DAS.Zendesk.Monitor.UnitTests/FunctionTests/NotifyTicketValidatorTests.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using FluentValidation;
+using Xunit;
+using ZenWatchFunction;
+
+namespace SFA.DAS.Zendesk.Monitor.UnitTests.FunctionTests
+{
+    public class NotifyTicketValidatorTests
+    {
+        private readonly NotifyTicketValidator _validator = new NotifyTicketValidator();
+
+        [Fact]
+        public void Should_Return_Error_When_Id_Is_Zero()
+        {
+            var model = new NotifyTicket { Id = 0 };
+
+            var result = _validator.Validate(model);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(e => e.PropertyName == nameof(NotifyTicket.Id));
+        }
+
+        [Fact]
+        public void Should_Pass_Validation_When_Id_Is_Positive()
+        {
+            var model = new NotifyTicket { Id = 12345 };
+
+            var result = _validator.Validate(model);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_Return_Error_When_Model_Is_Null()
+        {
+            NotifyTicket? model = null;
+
+            var action = () => _validator.Validate(model!);
+
+            action.Should().Throw<ValidationException>();
+        }
+
+        [Fact]
+        public void Should_Return_Single_Error_When_Id_Is_Zero()
+        {
+            var model = new NotifyTicket { Id = 0 };
+
+            var result = _validator.Validate(model);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].PropertyName.Should().Be(nameof(NotifyTicket.Id));
+            result.Errors[0].ErrorMessage.Should().Contain("must not be empty");
+        }
+    }
+}
